37769c3a9557f96d75710beec73fcf4282912760
bogdanmursa <bogdanmursa@yahoo.com>
2014-12-10 22:56:51 +0200

    Created Human Model

--- inTigaie_ro/models.py
+++ inTigaie_ro/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Human():
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def get_first_name(self):
+        return self.first_name
+
+    def get_last_name(self):
+        return self.last_name
+
+    def set_first_name(self, first_name):
+        self.first_name = first_name
+
+    def set_last_name(self, last_name):
+        self.last_name = last_name
+
+    def to_string(self):
+        return self.last_name + " " + self.first_name
