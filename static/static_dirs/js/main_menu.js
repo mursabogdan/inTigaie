c8f452afbce71d1cce208d438bb3b4240a5770c2
bogdanmursa <bogdanmursa@yahoo.com>
2015-01-25 21:57:28 +0200

    Drag & Drop works as I expected ( Guuuddd ) As well I made the restore part, when you double click on an ingredient he will got back to its initial place

--- static/static_dirs/js/main_menu.js
+++ static/static_dirs/js/main_menu.js
@@ -7,17 +7,47 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+    var ingredient = ev.target.id;
+    ev.dataTransfer.setData("text", ingredient);
+    ev.dataTransfer.setDragImage(document.getElementById(ingredient),0,0);
 }
 
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
+    var code = document.getElementById(data).src;
     ev.target.appendChild(document.getElementById(data));
+
+    var img = document.createElement("IMG");
+    img.setAttribute("id", data);
+    img.setAttribute("class", 'ingredientsPanel');
+    img.setAttribute("src", code);
+    img.setAttribute("width", "70");
+    img.setAttribute("width", "70");
+    img.setAttribute("draggable", "false");
+    img.setAttribute("ondblclick", "restoreDefaults('"+ data +"')")
+    document.getElementById('ingredientsPanel').appendChild(img);
 }
 
-function restoreDefaults(ev){
-    code = document.getElementById('leftBox').innerHTML;
-    document.getElementById('rightBox').innerHTML = code;
-    document.getElementById('leftBox').innerHTML = '';
+function restoreDefaults(id) {
+    var code = document.getElementById(id).src;
+
+    var img = document.createElement("IMG");
+    img.setAttribute("id", id);
+    img.setAttribute("class", 'ingredients');
+    img.setAttribute("draggable", "true");
+    img.setAttribute("ondragstart", "drag(event)");
+    img.setAttribute("src", code);
+    img.setAttribute("width", "70");
+    img.setAttribute("width", "70");
+
+    var elements = document.getElementById("ingredientsPanel").getElementsByClassName("ingredientsPanel");
+    var panel = document.getElementById("ingredientsPanel");
+    for(var i=0; i<elements.length; i++) {
+        if(elements[i].id == id ){
+            panel.removeChild(elements[i]);
+        }
+    }
+
+    document.getElementById('leftBox').appendChild(img);
 }
